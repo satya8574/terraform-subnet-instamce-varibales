3fdefdd4da0cdd9f1d82007d8fbf3660cedd6f46
SATYA PRKASH <96809659+satya8574@users.noreply.github.com>
2022-08-27 23:59:21 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "ap-south-1"
   access_key = "AKIA54J4UVX32QJQE26JS"
-  secret_key = "5TTaGm+Gpwo7W3V1QUIUdhR8vmR8NkGkJ5AbjC/wS"
+  secret_key = "5TTaGm+Gpwo7W3dxfdgfhtftreFV1QUIUdhR8vmR8NkGkJ5AbjC/wS"
 }
 
 
