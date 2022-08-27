c88e2605a1ddda7e28db3a7ddc1552399b9e40b3
SATYA PRKASH <96809659+satya8574@users.noreply.github.com>
2022-08-27 23:42:21 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "subnet_name" {
+
+type = string
+default = "subnet-2-ap-south-1b"
+}
+
+
+variable "kep_pair" {
+  description = "this is key name"
+  type = string
+  default = "dev27"
+}
